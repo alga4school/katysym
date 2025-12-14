c6e0425d881f568217768f8ac3992b29c3e3809a
alga4school <alga4school@gmail.com>
2025-12-14 20:21:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ async function apiGet(mode, params = {}) {
 async function apiPost(body) {
   const r = await fetch(WEBAPP_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },// ✅ МІНЕ ОСЫ
+    headers: { "Content-Type": "text/plain;charset=utf-8" }// ✅ МІНЕ ОСЫ
     body: JSON.stringify(body),
   });
   const text = await r.text();
@@ -641,6 +641,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Apply initial i18n once DOM ready
   applyI18n();
 });
+
 
 
 
